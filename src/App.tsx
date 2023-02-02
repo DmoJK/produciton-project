d08a147b17853ea3e6fa8bc3da47148c6236d642
DmoJK <l1sharook@mail.ru>
2023-02-02 13:50:04 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Suspense } from "react";
+import { Link } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import "./styles/index.scss";
+import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
+import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
+import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
+
+
+
+export const App = () => {
+  const {theme, toggleTheme} = useTheme()
+
+  return (
+    <div className={classNames('app', {}, [theme])}>
+      <button onClick={toggleTheme}>TOGGLE THEME</button>
+      <Link to={"/"}>Главная страница</Link>
+      <br />
+      <Link to={"/about"}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/about" element={<AboutPageLazy />} />
+          <Route path="/" element={<MainPageLazy />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+};
