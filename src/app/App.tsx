fe345de86a51e7c7788c696fe3aeaf79d7fad6ae
DmoJK <l1sharook@mail.ru>
2023-02-22 12:11:37 +0300

    fix theme

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,3 @@
-import { useTheme } from "app/providers/ThemeProvider/lib/useTheme"
 import { classNames } from "shared/lib/classNames/classNames"
 import { Navbar } from "widgets/Navbar"
 import { Sidebar } from "widgets/Sidebar"
@@ -6,9 +5,8 @@ import { Suspense } from "react"
 import { AppRouter } from "./providers/router"
 
 export const App = () => {
-  const { theme } = useTheme()
   return (
-    <div className={classNames("app", {}, [theme])}>
+    <div className={classNames("app", {}, [])}>
       <Suspense fallback="">
         <Navbar />
         <div className="content-page">
--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -24,6 +24,7 @@ export const ThemeProvider: FC<ThemeProviderProps> = ({
     }),
     [theme]
   )
+  document.body.className = theme
 
   return (
     <ThemeContext.Provider value={defaultProps}>
