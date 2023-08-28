12fa9cbfe1ee795454f11d87ace5f35081c6d26d
DmoJK <l1sharook@mail.ru>
2023-08-28 18:32:22 +0300

    add babel, configure babel for typescript, add fork, ts checker, add custom babel plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,10 +2,12 @@ import webpack from "webpack"
 import { buildCssLoader } from "./loaders/buildCssLoader"
 import { buildSvgLoader } from "./loaders/buildSvgLoader"
 import { BuildOptions } from "./types/config"
+import { buildBabelLoader } from "./loaders/buildBabelLoader"
 
-export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+  const { isDev } = options
+
   const svgLoader = buildSvgLoader()
-
   const fileLoader = {
     test: /\.(png|jpe?g|gif|woff|woff2)$/i,
     use: [
@@ -14,14 +16,23 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
       },
     ],
   }
+  const codeBabelLoader = buildBabelLoader({ ...options, isTsx: false })
+  const tsxCodeBabelLoader = buildBabelLoader({ ...options, isTsx: true })
 
   const cssLoaders = buildCssLoader(isDev)
 
-  const typescriptLoader = {
-    test: /\.tsx?$/,
-    use: "ts-loader",
-    exclude: /node_modules/,
-  }
+  // const typescriptLoader = {
+  //   test: /\.tsx?$/,
+  //   use: "ts-loader",
+  //   exclude: /node_modules/,
+  // }
 
-  return [fileLoader, svgLoader, typescriptLoader, cssLoaders]
+  return [
+    fileLoader,
+    svgLoader,
+    codeBabelLoader,
+    tsxCodeBabelLoader,
+    // typescriptLoader,
+    cssLoaders,
+  ]
 }
