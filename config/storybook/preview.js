1ec4d0151d06ad9ebb892dbbafe998dc6fd5fa5a
DmoJK <l1sharook@mail.ru>
2023-09-16 18:10:47 +0300

    fix storybook import theme

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -4,7 +4,7 @@ import { StyleDecorator } from "../../src/shared/config/storybook/StyleDecorator
 import { ThemeDecorator } from "../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator"
 import { RouteDecorator } from "../../src/shared/config/storybook/RouteDecorator/RouteDecorator"
 import { SuspenseDecorator } from "../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator"
-import { Theme } from "../../src/app/providers/ThemeProvider"
+import { Theme } from "../../src/shared/const/theme"
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
