4b6e2f785cde197b5fe5732324bd212a85d8eaca
DmoJK <l1sharook@mail.ru>
2023-09-18 13:56:52 +0300

    add typedMemo

--- src/shared/ui/Select/Select.tsx
+++ src/shared/ui/Select/Select.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent, useMemo } from "react"
 
+import { typedMemo } from "@/shared/const/typedMemo"
 import { classNames, Mods } from "@/shared/lib/classNames/classNames"
 
 import cls from "./Select.module.scss"
@@ -18,40 +19,42 @@ interface SelectProps<T extends string> {
   readonly?: boolean
 }
 
-export const Select = <T extends string>({
-  className,
-  label,
-  options,
-  value,
-  onChange,
-  readonly,
-}: SelectProps<T>) => {
-  const optionsList = useMemo(() => {
-    return options?.map((opt) => (
-      <option className={cls.option} value={opt.value} key={opt.value}>
-        {opt.content}
-      </option>
-    ))
-  }, [options])
-  const onChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
-    onChange?.(e.target.value as T)
+export const Select = typedMemo(
+  <T extends string>({
+    className,
+    label,
+    options,
+    value,
+    onChange,
+    readonly,
+  }: SelectProps<T>) => {
+    const optionsList = useMemo(() => {
+      return options?.map((opt) => (
+        <option className={cls.option} value={opt.value} key={opt.value}>
+          {opt.content}
+        </option>
+      ))
+    }, [options])
+    const onChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+      onChange?.(e.target.value as T)
+    }
+    const mods: Mods = {}
+    return (
+      <div className={classNames(cls.Wrapper, mods, [className])}>
+        {label && (
+          <span className={readonly ? cls.labelDisabled : cls.label}>
+            {label}
+          </span>
+        )}
+        <select
+          className={cls.select}
+          value={value}
+          onChange={onChangeHandler}
+          disabled={readonly}
+        >
+          {optionsList}
+        </select>
+      </div>
+    )
   }
-  const mods: Mods = {}
-  return (
-    <div className={classNames(cls.Wrapper, mods, [className])}>
-      {label && (
-        <span className={readonly ? cls.labelDisabled : cls.label}>
-          {label}
-        </span>
-      )}
-      <select
-        className={cls.select}
-        value={value}
-        onChange={onChangeHandler}
-        disabled={readonly}
-      >
-        {optionsList}
-      </select>
-    </div>
-  )
-}
+)
